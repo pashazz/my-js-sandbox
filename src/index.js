9f7116586de821aaa4c7d4b2fa1dad27df8161f0
Pavel Zinin <pzinin@gmail.com>
2018-04-11 15:44:05 +0300

    TableHeader

--- src/index.js
+++ src/index.js
@@ -7,11 +7,29 @@ const StyledTest = styled.button.attrs({ className: "btn btn-secondary" })`
   color: #333;
   background: #f5f5f5;
 `;
+const PlainTableHeader = ({ className, column, children }) => {
+  return (
+    <th className={className} key={column}>
+      {children}
+    </th>
+  );
+};
 
-const App = () => (
-  <StyledTest>
-  X
-  </StyledTest>
-);
+const SortedTableHeader = styled(PlainTableHeader).attrs({
+  className: "header"
+})`
+  color: #333;
+  background: #f5f5f5;
+`;
+
+const TableHeader = ({ column, sort, children }) => {
+  return sort && sort.column === column ? (
+    <SortedTableHeader> {children} </SortedTableHeader>
+  ) : (
+    <PlainTableHeader> {children} </PlainTableHeader>
+  );
+};
+
+const App = () => <TableHeader>Pizda</TableHeader>;
 
 render(<App />, document.getElementById("root"));
